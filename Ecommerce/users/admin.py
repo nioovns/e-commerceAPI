07c77d2a3341e395c6d3e24ac28caaa61b996fdd
newsha varnaseri <nioshavarnaseri@gmail.com>
2026-09-10 02:03:07 +0330

    create custom user model

--- Ecommerce/users/admin.py
+++ Ecommerce/users/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+
+    list_display = (
+        "email",
+        "first_name",
+        "last_name",
+        "phone_number",
+        "is_staff",
+        "is_active",
+    )
+
+    ordering = ("email",)
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {
+            "fields": (
+                "first_name",
+                "last_name",
+                "phone_number",
+                "address",
+            )
+        }),
+        ("Permissions", {
+            "fields": (
+                "is_active",
+                "is_staff",
+                "is_superuser",
+                "groups",
+                "user_permissions",
+            )
+        }),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email",
+                "phone_number",
+                "password1",
+                "password2",
+            ),
+        }),
+    )
